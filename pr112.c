1888fdef210193dfefe3ed08de532ec6836128e0
kirtan1432 <133342879+kirtan1432@users.noreply.github.com>
2023-07-04 12:44:30 +0530

    pr112.c

--- pr112.c
+++ pr112.c
@@ -1,3 +1,21 @@
+/*
+ Write a Program to find cubes of all elements from a given 2D array using Pointer with UDF.
+For example,
+Input:
+Enter array's size: 2
+
+Enter array elements:
+a[0][0] = 3
+a[0][1] = 2
+a[1][0] = 5
+a[1][1] = 4
+
+Output:
+Cubes of all elements:
+9   4
+25 64
+*/
+
 #include<stdio.h>
 
 void kirtan(int n, int m) {
