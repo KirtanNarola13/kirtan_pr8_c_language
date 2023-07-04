bd19b31a93dd67c1cfd383a6e780c796333235d7
kirtan1432 <133342879+kirtan1432@users.noreply.github.com>
2023-07-04 12:24:04 +0530

    PR8

--- pr112.c
+++ pr112.c
@@ -1,29 +1,34 @@
 #include<stdio.h>
 
-void main (){
-	int n,m;
-	printf("Enter the size of row : ");
-	scanf("%d",&n);
-	printf("Enter the size of row : ");
-	scanf("%d",&m);
-	int a[n][m];
-	int i,j;
-	for(i=0;i<n;i++){
-		for(j=0;j<m;j++){
-		
-		printf("a[%d][%d] => ",i,j);
-		scanf("%d",&a[i][j]);
-	}
-	}
-	int *k;
-	for(i=0;i<n;i++){
-		for(j=0;j<m;j++){
-		
-		a[i][j]=a[i][j]*a[i][j]*a[i][j];
-		k=&a[i][j];
-		printf("%d\n",*k);
-	}
-	}
+void kirtan(int n, int m) {
+    int a[n][m];
+    int i, j;
 
-	
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            printf("a[%d][%d] => ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int *k;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            a[i][j] = a[i][j] * a[i][j] * a[i][j];
+            k = &a[i][j];
+            printf("%d\n", *k);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    printf("Enter the size of row: ");
+    scanf("%d", &n);
+    printf("Enter the size of column: ");
+    scanf("%d", &m);
+
+    kirtan(n, m);
+
+    return 0;
 }
